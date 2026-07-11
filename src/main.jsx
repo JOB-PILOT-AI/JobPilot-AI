197d01d79d129ddec2cea4c2ad0d300b9730f8cb
Ankit24072002 <kumaranikant24@gmail.com>
2026-07-11 13:33:13 +0530

    fix: upgrade react-query to @tanstack/react-query for React 19 compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { QueryClientProvider, QueryClient } from 'react-query'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import './lib/setupApi'
 import App from './App'
 import './styles/globals.css'
